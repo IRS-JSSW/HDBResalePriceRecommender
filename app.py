f85890f7121452214f1caa84b98514f0fdd541b4
Jovin <jovintoh88sp@gmail.com>
2021-03-11 22:11:06 +0800

    Modifed home.html, added completion year

--- app.py
+++ app.py
@@ -7,16 +7,14 @@ app.config['SECRET_KEY'] = 'e7d0eaa8168231567e93d4eea521188dbb83b37ccaf576190862
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    form = SearchResaleHDBForm()
+    return render_template('home.html', form=form)
 
 @app.route('/about')
 def about():
     return render_template('about.html', title='About Page')
 
-@app.route('/search')
-def search():
-    form = SearchResaleHDBForm()
-    return render_template('search.html', title='Search Resale HDB', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,9 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SubmitField
 from wtforms.validators import DataRequired, Length
+import datetime
 
 class SearchResaleHDBForm(FlaskForm):
     streetname = StringField('Street Name', validators=[DataRequired(), Length(min=2, max=20)])
-    dropdown_list = ['1', '2', '3', '4', '5']
-    bedrooms = SelectField('Bedrooms', choices=dropdown_list, default=1)
+    dropdown_list_bedrooms = ['1', '2', '3', '4', '5']
+    bedrooms = SelectField('Bedrooms', choices=dropdown_list_bedrooms, default=1)
+    dropdown_list_completion = list(range(1960, datetime.datetime.today().year))
+    completion = SelectField('Year of completion', choices=dropdown_list_completion, default=dropdown_list_completion[-1])
     submit = SubmitField('Search')
